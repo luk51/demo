dab65f26e8b2e99f6a2af87c221625c6357a3dde
Lukas Kitsche <lukaskitsche@posteo.de>
2022-06-12 00:27:40 +0200

    setzen eines buchstabens im ersten label

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label cell_00;
+    @FXML
+    public void setCell_00(String letter) {
+        cell_00.setText(letter);
+    }
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        cell_00.setText("S");
     }
 }
